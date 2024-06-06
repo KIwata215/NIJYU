2c6f1a246a496da0bd295fa5e23d80860031e7d1
aaaa <2220140@ecc.ac.jp>
2024-06-06 11:17:27 +0900

    アカウント新規登録画面２枚目

--- lib/register2.dart
+++ lib/register2.dart
@@ -39,124 +39,122 @@ class _RegisterPageState extends State<Register2Page> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Container(
-        ////////グラデーション
-        // decoration: const BoxDecoration(
-        //   gradient: LinearGradient(
-        //     begin: Alignment.topLeft,
-        //     end: Alignment.bottomRight,
-        //     stops: [0.1, 0.9],
-        //     colors: [
-        //       Color.fromRGBO(64, 129, 137, 1), // グラデーションの開始色
-        //       Color.fromRGBO(92, 160, 128, 1), // グラデーションの終了色
-        //     ],
-        //   ),
-        // ),
         child: Scaffold(
-          body: Container(
-        // decoration: BoxDecoration(
-        //   gradient: LinearGradient(
-        //     colors: [Colors.purple, Colors.blue],
-        //     begin: Alignment.topCenter,
-        //     end: Alignment.bottomCenter,
-        //   ),
-        // ),
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: '年'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return '年を入力してください';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        year = value ?? '';
-                      },
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: '月'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return '月を入力してください';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        month = value ?? '';
-                      },
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Expanded(
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: '日'),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return '日を入力してください';
-                        }
-                        return null;
-                      },
-                      onSaved: (value) {
-                        day = value ?? '';
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'パスワード'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'パスワードを入力してください';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  password = value ?? '';
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'パスワード（確認用）'),
-                obscureText: true,
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return '確認用パスワードを入力してください';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  confirmPassword = value ?? '';
-                },
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState?.validate() ?? false) {
-                    _formKey.currentState?.save();
-                    // 次の画面に遷移
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(builder: (context) => NextPage()),
-                    // );
-                  }
-                },
-                child: Text('次へ'),
-              ),
-            ],
+          appBar: AppBar(
+            title: Center(
+              child: Text("アカウント新規登録"),
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
           ),
-        ),
-      ),
+
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0), // 左右のパディング
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 30.0), // フィールド間のパディング
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: '年'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '年を入力してください';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            year = value ?? '';
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0), // フィールド間のパディング
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: '月'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '月を入力してください';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            year = value ?? '';
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 30.0), // フィールド間のパディング
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: '日'),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '日を入力してください';
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            year = value ?? '';
+                          },
+                        ),
+                      ),
+                    ),
+                  ]
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'パスワード'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'パスワードを入力してください';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    year = value ?? '';
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'パスワード(確認)'),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'パスワード(確認)を入力してください';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    year = value ?? '';
+                  },
+                ),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState?.validate() ?? false) {
+                        _formKey.currentState?.save();
+                        // 次の画面に遷移
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => ///NextPage///()),
+                        // );
+                      }
+                    },
+                    child: Text("次へ"),
+                  )
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
