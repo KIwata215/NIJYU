820aebb9e7d184210bf99bf67b511d8d621b9b11
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-07-01 14:52:17 +0900

    update:データクラスの内容を追加しました

--- lib/constants/player.dart
+++ lib/constants/player.dart
@@ -1,12 +1,14 @@
 class Player {
-  String name;
-  String category;
-  int score;
+  String name;//名前
+  String category;//飲むお酒
+  int score;//今までの合計スコア
   int remainingMath; // 残りマス数
+  int plusscore;
 
   Player(
-      {required this.name,
-      required this.category,
-      required this.score,
-      required this.remainingMath});
+      {required this.name,  
+      required this.category, 
+      required this.score,  
+      required this.remainingMath,
+      required this.plusscore});
 }
