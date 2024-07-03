680bd1950605e678d118ca939b11ff6c0d2ae1d3
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-07-03 20:04:12 +0900

    add:コード更新

--- lib/constants/player.dart
+++ lib/constants/player.dart
@@ -1,14 +1,22 @@
-class Player {
-  String name;//名前
-  String category;//飲むお酒
-  int score;//今までの合計スコア
-  int remainingMath; // 残りマス数
-  int plusscore;
+import 'package:flutter/material.dart';
+import 'package:nijyu/constants/colors.dart';
 
-  Player(
-      {required this.name,  
-      required this.category, 
-      required this.score,  
-      required this.remainingMath,
-      required this.plusscore});
+class Player {
+  String name; //名前
+  String category; //飲むお酒
+  int score; //今までの合計スコア
+  int remainingMath; // 残りマス数
+  int plusScore;
+  int position; // 場所
+  Color color = colors.playerInfo;
+
+  Player({
+    required this.name,
+    required this.category,
+    required this.score,
+    required this.remainingMath,
+    required this.plusScore,
+    this.position = 0,
+    required this.color,
+  });
 }
