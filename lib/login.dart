2a2b5345a9ae8e69e95d97e3e3df4c89a0b1ea8b
KIwata215 <106150270+KIwata215@users.noreply.github.com>
2024-07-30 22:57:43 +0900

    update:appleIDでのログインを削除しました

--- lib/login.dart
+++ lib/login.dart
@@ -3,18 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import "package:bordered_text/bordered_text.dart";
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:nijyu/component/buttton.dart';
 import 'package:nijyu/email_login.dart';
 import 'package:nijyu/test_nextPage.dart';
 
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+class Login extends StatefulWidget {
 
   @override
-  State<MyHomePage> createState() => Loginpage();
+  State<Login> createState() => Loginpage();
 }
 
-class Loginpage extends State<MyHomePage> {
+class Loginpage extends State<Login> {
 
   Future<UserCredential> signInWithGoogle() async {
   // Trigger the authentication flow
@@ -53,25 +53,36 @@ class Loginpage extends State<MyHomePage> {
         ),
       ),
       child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100.0.h),
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.h),
+            child: AppBar(
+              backgroundColor: Colors.transparent,
+              iconTheme: IconThemeData(
+              color: Colors.white
+              ),
+              centerTitle: true,
+              title: BorderedText(
+                strokeWidth: 3.0.sp, // 縁の太さ
+                strokeColor: Colors.black, // 縁の色
+                child: Text(
+                  "ログイン",
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    color: Colors.white, // テキストの色
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ),
+            )
+          ),
+       ),
         backgroundColor: Colors.transparent, // 背景色を透明にする
         body: Center(
           child: Column(
             children: [
-              Padding(
-                padding: EdgeInsets.only(top: 48.h, bottom: 35.h),
-                child: BorderedText(
-                  strokeWidth: 3.0.sp, // 縁の太さ
-                  strokeColor: Colors.black, // 縁の色
-                  child: Text(
-                    'ログイン',
-                    style: TextStyle(
-                      fontSize: 18.sp,
-                      color: Colors.white, // テキストの色
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
+              SizedBox(height: 40.h,),
               Padding(
                 padding: EdgeInsets.only(bottom: 25.h),
                 child: BorderedText(
@@ -88,7 +99,7 @@ class Loginpage extends State<MyHomePage> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(bottom: 15.h),
+                padding: EdgeInsets.only(bottom: 30.h),
                 child: Button(
                   height: 45,
                   width: 400,
@@ -107,7 +118,7 @@ class Loginpage extends State<MyHomePage> {
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(bottom: 15.h),
+                padding: EdgeInsets.only(bottom: 30.h),
                 child: Button(
                   height: 45,
                   width: 400,
@@ -142,25 +153,6 @@ class Loginpage extends State<MyHomePage> {
                   image: 'assets/images/Googleicon.png',
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(bottom: 5.h),
-                child: Button(
-                  height: 45,
-                  width: 400,
-                  backgroundColor: Colors.black,
-                  text: 'Apple',
-                  textColor: Colors.white,
-                  onpress: () {
-                    // Navigator.push(
-                    //     context,
-                    //     MaterialPageRoute(
-                    //         builder: (context) => Register2Page()));
-                  },
-                  iconheight: 75.h,
-                  iconwidth: 75.h,
-                  image: 'assets/images/appleicon.png',
-                ),
-              ),
             ],
           ),
         ),
@@ -169,66 +161,5 @@ class Loginpage extends State<MyHomePage> {
   }
 }
 
-class Button extends StatelessWidget {
-  final String text;
-  final GestureTapCallback onpress;
-  final double height;
-  final double width;
-  final double iconheight;
-  final double iconwidth;
-  final String image;
-  final Color backgroundColor; // 背景色プロパティ
-  final Color textColor; // 文字色プロパティ
 
-  Button({
-    required this.text,
-    required this.onpress,
-    required this.height,
-    required this.width,
-    required this.iconheight,
-    required this.iconwidth,
-    required this.image,
-    required this.backgroundColor, // 背景色を受け取る
-    required this.textColor, // 文字色を受け取る
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      height: height,
-      child: ElevatedButton(
-        onPressed: onpress,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor, // 背景色を設定
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12.r), // ボタンの角の丸み
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start, // アイコンを左寄せ
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              image,
-              width: iconwidth,
-              height: iconheight,
-            ),
-            Expanded(
-              child: Text(
-                text,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 10.sp,
-                  color: textColor, // 文字色を設定
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
 
